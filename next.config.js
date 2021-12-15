e09b9751e6f5f38af75f832f2897aca07f248894
Tiago Gimenes <tlgimenes@gmail.com>
2021-12-15 14:25:11 -0300

    add more files

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-module.exports = {
+const withBundleStats = require('next-plugin-bundle-stats')
+
+module.exports = withBundleStats({
   images: {
     domains: [
       'storeframework.vtexassets.com',
       'storecomponents.vtexassets.com',
     ],
   },
-}
+})
