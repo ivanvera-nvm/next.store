00d9edd399f64f84a09163b5516548ce18c3c970
Tiago Gimenes <tlgimenes@gmail.com>
2021-12-15 17:28:39 -0300

    use envelop

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,20 +1,20 @@
-// import {
-//   envelop,
-//   useExtendContext,
-//   useMaskedErrors,
-//   useSchema,
-// } from '@envelop/core'
-// import { useGraphQlJit } from '@envelop/graphql-jit'
-// import { useParserCache } from '@envelop/parser-cache'
-// import { useValidationCache } from '@envelop/validation-cache'
+/* eslint-disable react-hooks/rules-of-hooks */
+import {
+  envelop,
+  useExtendContext,
+  useMaskedErrors,
+  useSchema,
+} from '@envelop/core'
+import { useGraphQlJit } from '@envelop/graphql-jit'
+import { useParserCache } from '@envelop/parser-cache'
+import { useValidationCache } from '@envelop/validation-cache'
 import {
   getContextFactory as storeApiGetContextFactory,
   getSchema as storeApiGetSchema,
 } from '@faststore/api'
-// import { GraphQLError, execute as run, parse } from 'graphql'
-import { execute as run, parse } from 'graphql'
+import { GraphQLError } from 'graphql'
 import type { ExecutionResult } from 'graphql'
-// import type { FormatErrorHandler } from '@envelop/core'
+import type { FormatErrorHandler } from '@envelop/core'
 
 import persisted from '../../@generated/graphql/persisted.json'
 import storeConfig from '../../store.config'
@@ -31,35 +31,35 @@ const apiOptions = {
 const apiSchema = storeApiGetSchema(apiOptions)
 const apiContextFactory = storeApiGetContextFactory(apiOptions)
 
-// const isBadRequestError = (err: GraphQLError) => {
-//   return err.originalError && err.originalError.name === 'BadRequestError'
-// }
+const isBadRequestError = (err: GraphQLError) => {
+  return err.originalError && err.originalError.name === 'BadRequestError'
+}
 
-// const maskError: FormatErrorHandler = (err: GraphQLError | unknown) => {
-//   if (err instanceof GraphQLError) {
-//     if (!isBadRequestError(err)) {
-//       return new GraphQLError('Sorry, something went wrong.')
-//     }
+const maskError: FormatErrorHandler = (err: GraphQLError | unknown) => {
+  if (err instanceof GraphQLError) {
+    if (!isBadRequestError(err)) {
+      return new GraphQLError('Sorry, something went wrong.')
+    }
 
-//     return err
-//   }
+    return err
+  }
 
-//   return new GraphQLError('Sorry, something went wrong.')
-// }
+  return new GraphQLError('Sorry, something went wrong.')
+}
 
-// const getEnvelop = async (formatError: FormatErrorHandler) =>
-//   envelop({
-//     plugins: [
-//       useSchema(await apiSchema),
-//       useExtendContext(apiContextFactory),
-//       useMaskedErrors({ formatError }),
-//       useGraphQlJit(),
-//       useValidationCache(),
-//       useParserCache(),
-//     ],
-//   })
+const getEnvelop = async (formatError: FormatErrorHandler) =>
+  envelop({
+    plugins: [
+      useSchema(await apiSchema),
+      useExtendContext(apiContextFactory),
+      useMaskedErrors({ formatError }),
+      useGraphQlJit(),
+      useValidationCache(),
+      useParserCache(),
+    ],
+  })
 
-// const envelopPromise = getEnvelop(maskError)
+const envelopPromise = getEnvelop(maskError)
 
 export const execute = async <
   D extends Record<string, unknown>,
@@ -79,15 +79,13 @@ export const execute = async <
     throw new Error(`No query found for operationName: ${operationName}`)
   }
 
-  // const enveloped = await envelopPromise
-  // const {
-  //   parse,
-  //   contextFactory,
-  //   execute: run,
-  //   schema,
-  // } = enveloped(envelopContext)
-  const schema = await apiSchema
-  const contextFactory = apiContextFactory
+  const enveloped = await envelopPromise
+  const {
+    parse,
+    contextFactory,
+    execute: run,
+    schema,
+  } = enveloped(envelopContext)
 
   return run({
     schema,
